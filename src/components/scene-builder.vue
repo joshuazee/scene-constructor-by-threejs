6d17380c2a32b5c019b6670c7399db18f22fa61b
sss <joshua127@outlook.com>
2023-08-25 18:11:56 +0800

    update:

--- src/components/scene-builder.vue
+++ src/components/scene-builder.vue
@@ -1,26 +1,41 @@
 <template>
-  <div class="scene-builder-container">
-    <div class="scene-builder-icon">配置</div>
-    <transition name="animate-slide">
+  <transition name="animate-slide">
+    <div class="scene-builder-container" :key="animateKey">
+      <div class="scene-builder-icon" @click="handleChangePaneStatus">
+        <el-icon></el-icon>
+      </div>
       <div v-if="showPane" class="scene-builder-pane"></div>
-    </transition>
-  </div>
+    </div>
+  </transition>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
 
+const animateKey = ref(1);
 const showPane = ref(false);
+
+const handleChangePaneStatus = () => {
+  animateKey.value++;
+  showPane.value = !showPane.value;
+};
 </script>
 
 <style lang="less" scoped>
 .scene-builder-container {
+  display: flex;
   .scene-builder-icon {
     padding: 0.5em 1em;
     border-radius: 3px;
     background: @themeColor;
     color: #fff;
     cursor: pointer;
+  }
+
+  .scene-builder-pane {
+    background: #fff;
+    height: 80vh;
+    min-width: 400px;
   }
 }
 </style>
